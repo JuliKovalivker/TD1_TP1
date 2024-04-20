26095bdd8cbe5d849f87957696c8e02e723ed8f6
Benzagon <gonbenzaquen@gmail.com>
2024-04-19 21:12:09 -0300

    feat: test_cases

--- templates/distancia_binaria.py
+++ templates/distancia_binaria.py
@@ -2,7 +2,7 @@ def bin_misma_len(n:int, m:int) -> list[str]:
     '''
     Requiere: n y m > 0
     Devuelve: una lista con la representación binaria de n y m con la 
-    misma len compleada con 0s. La primera posicion de la lista corresponde
+    misma longitud, completada con 0s. La primera posicion de la lista corresponde
     a n y la segunda a m
     '''
     n_bin:str = bin(n).replace('0b', '')
@@ -18,7 +18,7 @@ def bin_misma_len(n:int, m:int) -> list[str]:
 
 def aledaños_en_intervalo(n:int, a:int, b:int) -> list[int]:
     '''
-    Requiere: a > 0, a < b, n > 0
+    Requiere: n > 0, a > 0, a < b
     Devuelve: los numeros en el intervalo [a, b] que
     son vecinos aledaños a n
     '''
@@ -53,7 +53,7 @@ def distancia_binaria(n:int, m:int) -> int:
 def son_aledaños(n:int, m:int) -> bool:
     '''
     Requiere: n y m > 0
-    Devuelve: True si son vecinos aledaño, False si no lo son
+    Devuelve: True si son vecinos aledaños, False si no lo son
     '''
     res:bool = False
     if distancia_binaria(n, m) == 1:
@@ -66,15 +66,14 @@ def aledaños_menores(n:int) -> list[int]:
     Requiere: n > 0
     Devuelve: los vecinos aledaños menores a n
     '''
-    res:list[int] = aledaños_en_intervalo(n, 0, n-1)
+    res:list[int] = aledaños_en_intervalo(n, 1, n-1)
     return res
-#COMPROBAR
 
 
 def cant_aledaños(n:int, a:int, b:int) -> int:
     '''
-    Requiere: a > 0, a < b, n > 0
-    Devuelve: la cantidad de numeros en el intervalo [a, b] que
+    Requiere: n > 0, a > 0, a < b
+    Devuelve: la cantidad de números en el intervalo [a, b] que
     son vecinos aledaños a n
     '''
     res:int = len(aledaños_en_intervalo(n, a, b))
@@ -83,10 +82,12 @@ def cant_aledaños(n:int, a:int, b:int) -> int:
 
 def densidad_intervalo(n:int, a:int, b:int) -> float:
     '''
-    Requiere: a > 0, a < b, n > 0
+    Requiere: n > 0, a > 0, a < b
     Devuelve: la densidad binaria de n en [a, b], con un error
     menor a 10^-5
     '''
-    res:float = cant_aledaños(n, a, b) / (b - a)
+    res:float = cant_aledaños(n, a, b) / (b - a + 1)
     res = round(res, 5)
     return res
+
+print(densidad_intervalo(1,2,3))
--- templates/distancia_binaria_tests.py
+++ templates/distancia_binaria_tests.py
@@ -1,19 +1,161 @@
 import unittest
 
 # Se importa el código a testear.
-from distancia_binaria import distancia_binaria, son_aledaños, aledaños_menores, cant_aledaños, densidad_intervalo
+from distancia_binaria import distancia_binaria, son_aledaños, aledaños_menores, cant_aledaños, densidad_intervalo, bin_misma_len, aledaños_en_intervalo
 
 #####################################################################
 
-class TestVecinosBinarios(unittest.TestCase):
-    ## ATENCION: los nombres de estas funciones deben empezar con test_
+# Tests para la función distancia_binaria()
+class TestDistanciaBinaria(unittest.TestCase):
 
-    def test_...(self):
-        ...
+    # Probar números con distinta cantidad de digitos en binario
+    def test_distintos_digitos(self):
+        output:int = distancia_binaria(1,128)
+        expected_output:int = 2
+        self.assertEqual(output,expected_output)
 
-    def test_...(self):
-        ...
+    # Probar el mismo numero
+    def test_mismo_numero(self):
+        output:int = distancia_binaria(128,128)
+        expected_output:int = 0
+        self.assertEqual(output,expected_output)
 
-## y asi con el resto de las funciones a testear.
+    # Ningún digito coincide
+    def test_distancia_completa(self):
+        output:int = distancia_binaria(127,128)
+        expected_output:int = 8
+        self.assertEqual(output,expected_output)
+
+# Tests para la función son_aledaños()
+class TestSonAledanos(unittest.TestCase):
+
+    # Probar 2 números aledaños
+    def test_aledanos_verdadero(self):
+        output:bool = son_aledaños(8,12)
+        expected_output:bool = True
+        self.assertEqual(output,expected_output)
+    
+    # Probar 2 números que no son aledaños
+    def test_aledanos_falso(self):
+        output:bool = son_aledaños(6,18)
+        expected_output:bool = False
+        self.assertEqual(output,expected_output)
+
+# Tests para la función aledaños_menores()
+class TestAledanosMenores(unittest.TestCase):
+
+    # Probar un número sin aledaños menores
+    def test_ningun_aledano(self):
+        output:list[int] = aledaños_menores(16)
+        expected_output:list[int] = []
+        self.assertEqual(output,expected_output)
+
+    # Probar un número con varios vecinos aledaños menores
+    def test_varios(self):
+        output:list[int] = aledaños_menores(11)
+        expected_output:list[int] = [3,9,10]
+        self.assertEqual(output,expected_output)
+
+# Tests para la función cant_aledaños()
+class TestCantidadAledanos(unittest.TestCase):
+
+    # Probar un número en el intervalo, con varios aledaños
+    def test_en_intervalo(self):
+        output:int = cant_aledaños(10,8,13)
+        expected_output:int = 2
+        self.assertEqual(output,expected_output)
+
+    # Probar un número sin ningún aledaño en ese intervalo
+    def test_sin_aledanos(self):
+        output:int = cant_aledaños(32,1,32)
+        expected_output:int = 0
+        self.assertEqual(output,expected_output)
+
+    # Probar un número menor que el intervalo, con aledaños en el intervalo
+    def test_menor(self):
+        output:int = cant_aledaños(1,4,32)
+        expected_output:int = 3
+        self.assertEqual(output,expected_output)
+
+    # Probar un número cuyos aledaños sean el último, y primer número del intervalo
+    def test_bordes(self):
+        output:int = cant_aledaños(33,1,32)
+        expected_output:int = 2
+        self.assertEqual(output,expected_output)
+
+# Tests para la función densidad_intervalo()
+class TestDensidadIntervalo(unittest.TestCase):
+
+    # Probar un intervalo cuya densidad binaria dé un número periódico
+    def test_periodico(self):
+        output:float = densidad_intervalo(10,8,13)
+        expected_output:float = 0.33333
+        self.assertEqual(output,expected_output)
+
+    # Probar la mitad de los números del intervalo son aledaños
+    def test_mitad_aledanos(self):
+        output:float = densidad_intervalo(1,2,3)
+        expected_output:float = 0.5
+        self.assertEquals(output,expected_output)
+
+    # Probar ningún número en el intervalo es aledaño
+    def test_ningun_aledano(self):
+        output:float = densidad_intervalo(32,1,31)
+        expected_output:float = 0.0
+        self.assertEqual(output,expected_output)
+
+# Tests para la función bin_misma_len()
+class TestBinMismaLen(unittest.TestCase):
+
+    # Probar 2 números con la misma cantidad de bits
+    def test_misma_len(self):
+        output:list[str] = bin_misma_len(2,3)
+        expected_output:list[str] = ['10','11']
+        self.assertEqual(output,expected_output)
+
+    # Probar 2 números con diferentes cantidades de bits
+    def test_distina_len(self):
+        output:list[str] = bin_misma_len(1,8)
+        expected_output:list[str] = ['0001','1000']
+        self.assertEqual(output,expected_output)
+
+    # Probar 2 números con diferentes cantidades de bits, pero arrancando con el mayor
+    def test_distina_len_mayor_primero(self):
+        output:list[str] = bin_misma_len(8,1)
+        expected_output:list[str] = ['1000','0001']
+        self.assertEqual(output,expected_output)
+
+    # Probar el mismo número, 2 veces
+    def test_distina_len(self):
+        output:list[str] = bin_misma_len(2,2)
+        expected_output:list[str] = ['10','10']
+        self.assertEqual(output,expected_output)
+
+# Tests para la función aledaños_en_intervalo()
+class TestAledanosEnIntervalo(unittest.TestCase):
+    # Probar ningún aledaño en el intervalo
+    def test_ninguno(self):
+        output:list[int] = aledaños_en_intervalo(32,1,31)
+        expected_output:list[int] = []
+        self.assertEqual(output,expected_output)
+
+    # Probar un intervalo vacío
+    def test_intervalo_vacio(self):
+        output:list[int] = aledaños_en_intervalo(1,2,2)
+        expected_output:list[int] = []
+        self.assertEqual(output,expected_output)
+        
+    # Probar algunos aledaños en el intervalo
+    def test_algunos(self):
+        output:list[int] = aledaños_en_intervalo(10,8,13)
+        expected_output:list[int] = [8,11]
+        self.assertEqual(output,expected_output)
+
+    # Probar un número cuyos aledaños sean el último, y primer número del intervalo
+    def test_bordes(self):
+        output:list[int] = aledaños_en_intervalo(33,1,32)
+        expected_output:list[int] = [1,32]
+        self.assertEqual(output,expected_output)
+
 
 unittest.main()
